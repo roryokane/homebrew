6885a760fbae7d3a48d4fabf968ddcc86e6eff3a
Rory O’Kane <rory@roryokane.com>
2014-09-11 20:22:01 -0400

    [WIP] start to set up Readable dependencies

--- Library/Formula/readable.rb
+++ Library/Formula/readable.rb
@@ -9,11 +9,18 @@ class Readable < Formula
   sha1 '273a7589f69fc82990dc4591eff1d4ab049edff1'
 
   depends_on 'guile'
+  depends_on 'clisp' # TODO or 'sbcl', or possibly other distributions - see readable's Makefile for what it *really* requires
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    # TODO detect if clisp and ASDF are already installed, and if so, configure readable to have support for them
+    system "./configure", "--without-clisp",
+                          "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
+  end
+
+  def caveats
+    "This formula does not install the Common Lisp version, because Homebrew currently cannot install ASDF if it is not installed."
   end
 
   def test
